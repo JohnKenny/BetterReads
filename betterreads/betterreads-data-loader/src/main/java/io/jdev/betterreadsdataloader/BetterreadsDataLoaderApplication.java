96ae0b024d61d791ab7f416cc3bb330f8f4ef191
JohnKenny <squareindigojohn@yahoo.ie>
2021-10-27 17:50:26 +0100

    books persist method added

--- betterreads/betterreads-data-loader/src/main/java/io/jdev/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ betterreads/betterreads-data-loader/src/main/java/io/jdev/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -4,10 +4,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import java.util.List;
+import java.util.ArrayList;
+//import java.util.AbstractList;
 
 import javax.annotation.PostConstruct;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +28,14 @@ import org.springframework.context.annotation.Bean;
 import connection.DataStaxAstraProperties;
 import io.jdev.betterreadsdataloader.author.Author;
 import io.jdev.betterreadsdataloader.author.AuthorRepository;
+import io.jdev.betterreadsdataloader.book.Book;
 
-@SpringBootApplication    
+@SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsDataLoaderApplication {
 
-	@Autowired AuthorRepository authorRepository;
+	@Autowired
+	AuthorRepository authorRepository;
 
 	@Value("${datadump.location.author}")
 	private String authorDumpLocation;
@@ -35,18 +44,17 @@ public class BetterreadsDataLoaderApplication {
 	private String worksDumpLocation;
 
 	public static void main(String[] args) {
-		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);   
+		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
 	}
 
-  
 	// initialises author values into cassandra db
 	public void initAuthors() {
 		Path path = Paths.get(authorDumpLocation);
-		try (Stream<String> lines = Files.lines(path)){
+		try (Stream<String> lines = Files.lines(path)) {
 			lines.limit(4).forEach(line -> {
 				// read and parse line
 				String jsonString = line.substring(line.indexOf("{"));
-				
+
 				try {
 					JSONObject jsonObject = new JSONObject(jsonString);
 					// construct Author object
@@ -58,14 +66,13 @@ public class BetterreadsDataLoaderApplication {
 					authorRepository.save(author);
 
 				} catch (JSONException e) {
-					
+
 					e.printStackTrace();
 				}
-				
 
 			});
 
-		} catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -73,18 +80,59 @@ public class BetterreadsDataLoaderApplication {
 	// init works values into cassandra db
 	private void initWorks() {
 		Path path = Paths.get(worksDumpLocation);
-		try(Stream<String> lines = Files.lines(path)){
+		try (Stream<String> lines = Files.lines(path)) {
 			lines.forEach(line -> {
 				String jsonString = line.substring(line.indexOf("{"));
 				try {
-					JSONObject jsonObject = new JSONObject(jsonString);  
+					JSONObject jsonObject = new JSONObject(jsonString);
+					// creates book object
+					Book book = new Book();
+					book.setId(jsonObject.getString("key").replace("/works/", ""));
 
-				} catch(JSONException e){
+					book.setName(jsonObject.optString("title"));
+					JSONObject descriptionObject = jsonObject.optJSONObject("description");
+					if (descriptionObject != null) {
+						book.setDescription(descriptionObject.optString("value"));
+					}
+					JSONObject publishedObj = jsonObject.optJSONObject("created");
+					if (publishedObj != null) {
+						String dateStr = publishedObj.getString("value");
+						book.setPublishedDate(LocalDate.parse(dateStr));
+					}
+
+					JSONArray coversJSONArray = jsonObject.optJSONArray("covers");
+					if (coversJSONArray != null) {
+						List<String> coverIds = new ArrayList<>();
+						for (int i = 0; i < coversJSONArray.length(); i++) {
+							coverIds.add(coversJSONArray.getString((i)));
+						}
+						book.setCoverIds(coverIds);
+					}
+
+					JSONArray authorsJSONArray = jsonObject.optJSONArray("authors");
+					if (authorsJSONArray != null) {
+						List<String> authorIds = new ArrayList<>();
+						for (int i = 0; i < authorsJSONArray.length(); i++) {
+							String authorId = authorsJSONArray.getJSONObject(i).getJSONObject("author").getString("key")
+									.replace("/authors/", "");
+							authorIds.add(authorId);
+						}
+						book.setAuthorIds(authorIds);
+						List<String> authorNames = authorIds.stream().map(id -> authorRepository.findById(id))
+								.map(optionalAuthor -> {
+									if (!optionalAuthor.isPresent())
+										return "Unknown Author";
+									return optionalAuthor.get().getName();
+								}).collect(Collectors.toList());
+						book.setAuthorNames(authorNames);
+					}
+
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			});
 
-		} catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
@@ -92,23 +140,16 @@ public class BetterreadsDataLoaderApplication {
 
 	// method runs on application start-up
 	@PostConstruct
-	public void start(){
+	public void start() {
 		initAuthors();
 		initWorks();
 	}
 
-	
-
-
-
-	
-
-
 	@Bean
-	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
 		Path bundle = astraProperties.getSecureConnectBundle().toPath();
 		return builder -> builder.withCloudSecureConnectBundle(bundle);
-		
+
 	}
 
 }
